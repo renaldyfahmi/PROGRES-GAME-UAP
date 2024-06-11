56bca34611ae26e4649fb6ec638416352594b90c
renaldyfahmi <152761798+renaldyfahmi@users.noreply.github.com>
2024-06-11 21:40:17 +0700

    Update sistem pemesanan tiket bioskop.cpp

--- sistem pemesanan tiket bioskop.cpp
+++ sistem pemesanan tiket bioskop.cpp
@@ -74,3 +74,67 @@ void tampilkanFilm() {
         printw("Judul: %s, Jadwal: %s\n", f.judul.c_str(), f.jadwal.c_str());
     }
 }
+void tambahFilm(const string &judul, const string &jadwal) {
+    film.emplace_back(judul, jadwal);
+}
+
+bool perbaruiJadwalFilm(const string &judul, const string &jadwalBaru) {
+    for (auto &f : film) {
+        if (f.judul == judul) {
+            f.jadwal = jadwalBaru;
+            printw("Jadwal film %s berhasil diperbarui!\n", judul.c_str());
+            return true;
+        }
+    }
+    printw("Film tidak ditemukan!\n");
+    return false;
+}
+
+void tampilkanTiket(const Pemesanan &pemesanan) {
+    printw("\n--- Tiket Bioskop ---\n");
+    printw("Nama Pengguna: %s\n", pemesanan.namaPengguna.c_str());
+    printw("Judul Film: %s\n", pemesanan.judulFilm.c_str());
+    printw("Jadwal: %s\n", pemesanan.jadwal.c_str());
+    printw("Nomor Kursi: %d\n", pemesanan.nomorKursi);
+    printw("Kode Konfirmasi: %s\n", pemesanan.kodeKonfirmasi.c_str());
+    printw("Harga Tiket: Rp 30,000\n");
+    printw("Barcode: ||| ||| ||| ||| |||\n");
+    printw("\n----------------------\n");
+}
+
+bool pesanTiket(const string &namaPengguna, const string &judulFilm, const string &jadwal, int nomorKursi) {
+    for (auto &k : kursiTersedia) {
+        if (k.first == judulFilm) {
+            auto &kursi = k.second;
+            auto it = find(kursi.begin(), kursi.end(), nomorKursi);
+            if (it != kursi.end()) {
+                kursi.erase(it);
+                string kodeKonfirmasi = "KONF" + to_string(pemesanan.size() + 1);
+                Pemesanan pemesananBaru(namaPengguna, judulFilm, jadwal, nomorKursi, kodeKonfirmasi);
+                pemesanan.push_back(pemesananBaru);
+                printw("Pemesanan berhasil! Kode Konfirmasi: %s\n", kodeKonfirmasi.c_str());
+                tampilkanTiket(pemesananBaru);
+                return true;
+            }
+        }
+    }
+    printw("Kursi tidak tersedia!\n");
+    return false;
+}
+
+bool filmTersedia(const string &judul, const string &jadwal) {
+    for (const auto &f : film) {
+        if (f.judul == judul && f.jadwal == jadwal) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void inputString(string &input, const char* prompt) {
+    printw("%s", prompt);
+    refresh();
+    char str[100];
+    getstr(str);
+    input = str;
+}
